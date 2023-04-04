748690459261cdbba274b8c6d03caab035e037c1
Derrick Hammer <derrick@derrickhammer.com>
2023-04-04 08:08:05 -0400

    *update dist

--- dist/index.js
+++ dist/index.js
@@ -163,7 +163,7 @@ export class Socket extends Client {
         this.eventUpdates[event].push(update);
     }
 }
-const MODULE = "_AXpXQ4czC7B0hcqXfbhGv5T5MFAFprS9KC2c39GYaj9hw";
+const MODULE = "_AXaAhoQvjUXiua5jXa-uPPwyOoV0JrRkKNBfKqWTCTf2A";
 export const createClient = factory(SwarmClient, MODULE);
 const socketFactory = factory(Socket, MODULE);
 const createSocket = async (...args) => {
